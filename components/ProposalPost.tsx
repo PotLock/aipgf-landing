ba92b46b11c53494b1a28d4ef75bd67f5c5204d7
louisdevzz <vohuunhan1310@gmail.com>
2024-09-23 02:12:02 +0700

    fix: deploy

--- components/ProposalPost.tsx
+++ components/ProposalPost.tsx
@@ -1,4 +1,4 @@
-import { NextPage } from "next";
+import type { NextPage } from "next";
 import Link from "next/link";
 import { useState,useEffect } from "react";
 import Tag from "./tag";
@@ -176,108 +176,121 @@ const ProposalPost: NextPage = () => {
         return str
     }
 
-    return(
-        proposals.length &&(
-            proposals.map((proposal)=>(
-                <Link 
-                    target="_blank" 
-                    href={`https://forum.aipgf.com/bos.forum.potlock.near/widget/app?page=proposal&id=${proposal.proposal_id}`} 
-                    key={proposal.proposal_id} 
-                    className="w-full border-aipgf-geyser border-[1px] border-solid box-border md:h-44 rounded-lg shadow-sm p-3 md:p-5 no-underline"
-                    style={{color: "unset"}}
-                    >
-                    <div className="flex md:flex-row flex-col gap-3 items-end md:items-center justify-between">
-                        <div className="flex flex-col md:flex-row md:gap-3 gap-1">
-                            <div className="flex flex-row gap-2 items-center md:items-start">
-                                <img width={30} className="rounded-full md:w-8 md:h-8 w-6 h-6" src="/assets/avatar.png" alt="avatar" />
-                                {
-                                    windowSize?.width <= 768&&(
-                                        <div className="flex flex-row gap-3">
-                                            <span className="text-sm md:text-lg font-semibold">{proposal.name}</span>
-                                            <div className="flex flex-row gap-1 items-center bg-[#F2CE8F] p-1 px-2 rounded-full">
-                                                <img width={windowSize?.width<768?12:14} src="/assets/icon/mvp.svg" alt="icon" />
-                                                <small className="text-[#24292F] text-[10px]">MVP</small>
+    const renderProposals = () => {
+        if (proposals.length === 0) {
+            return <div>Loading...</div>;
+        }
+
+        return(
+            proposals.length &&(
+                proposals.map((proposal)=>(
+                    <Link 
+                        target="_blank" 
+                        href={`https://forum.aipgf.com/bos.forum.potlock.near/widget/app?page=proposal&id=${proposal.proposal_id}`} 
+                        key={proposal.proposal_id} 
+                        className="w-full border-aipgf-geyser border-[1px] border-solid box-border md:h-44 rounded-lg shadow-sm p-3 md:p-5 no-underline"
+                        style={{color: "unset"}}
+                        >
+                        <div className="flex md:flex-row flex-col gap-3 items-end md:items-center justify-between">
+                            <div className="flex flex-col md:flex-row md:gap-3 gap-1">
+                                <div className="flex flex-row gap-2 items-center md:items-start">
+                                    <img width={30} className="rounded-full md:w-8 md:h-8 w-6 h-6" src="/assets/avatar.png" alt="avatar" />
+                                    {
+                                        windowSize?.width <= 768&&(
+                                            <div className="flex flex-row gap-3">
+                                                <span className="text-sm md:text-lg font-semibold">{proposal.name}</span>
+                                                <div className="flex flex-row gap-1 items-center bg-[#F2CE8F] p-1 px-2 rounded-full">
+                                                    <img width={windowSize?.width<768?12:14} src="/assets/icon/mvp.svg" alt="icon" />
+                                                    <small className="text-[#24292F] text-[10px]">MVP</small>
+                                                </div>
                                             </div>
-                                        </div>
-                                    )
-                                }
-                            </div>
-                            <div className="flex flex-col gap-1">
-                                {
-                                    windowSize?.width > 768 &&(
-                                        <div className="flex flex-row gap-3">
-                                            <span className="text-sm md:text-lg font-semibold">{proposal.name}</span>
-                                            {proposal.labels?.map((data) => (
-                                                <Tag
-                                                    key={data}
-                                                    propBackgroundColor={
-                                                        labelIcons[data]?.color ?? "#b7b7b7"
-                                                    }
-                                                    propWidth="max-content"
-                                                    x={labelIcons[data]?.icon ?? "icon.svg"}
-                                                    cancel={data}
-                                                    propFontWeight="unset"
-                                                    propColor={
-                                                        labelIcons[data]?.textColor ?? "#000"
-                                                    }
-                                                    cancelFontSize="0.75rem"
-                                                />
-                                            ))}
-                                        </div>
-                                    )
-                                }
-                                <div className="flex flex-col gap-2">
-                                    <small className="font-semibold flex flex-row gap-1 items-center mt-2">
-                                        By{" "}
-                                        {proposal?.author_id?.length > 20
-                                            ? `${proposal?.author_id?.slice(0, 20)}...`
-                                            : proposal?.author_id}
-                                        <span className="font-thin">| {timeAgo(proposal.ts)}</span>
-                                    </small>
-                                    <span className="md:text-base text-xs">{truncateString(proposal.summary)}</span>
+                                        )
+                                    }
                                 </div>
-                                <div className="flex flex-row gap-5 mt-3 items-center">
-                                    <div className="flex flex-row gap-1 items-center">
-                                        <img width={15} src="/assets/icon/heart.svg" alt="icon" />
-                                        <small className="text-[#E0023D] font-semibold text-xs">3 votes</small>
+                                <div className="flex flex-col gap-1">
+                                    {
+                                        windowSize?.width > 768 &&(
+                                            <div className="flex flex-row gap-3">
+                                                <span className="text-sm md:text-lg font-semibold">{proposal.name}</span>
+                                                {proposal.labels?.map((data) => (
+                                                    <Tag
+                                                        key={data}
+                                                        propBackgroundColor={
+                                                            labelIcons[data]?.color ?? "#b7b7b7"
+                                                        }
+                                                        propWidth="max-content"
+                                                        x={labelIcons[data]?.icon ?? "icon.svg"}
+                                                        cancel={data}
+                                                        propFontWeight="unset"
+                                                        propColor={
+                                                            labelIcons[data]?.textColor ?? "#000"
+                                                        }
+                                                        cancelFontSize="0.75rem"
+                                                    />
+                                                ))}
+                                            </div>
+                                        )
+                                    }
+                                    <div className="flex flex-col gap-2">
+                                        <small className="font-semibold flex flex-row gap-1 items-center mt-2">
+                                            By{" "}
+                                            {proposal?.author_id?.length > 20
+                                                ? `${proposal?.author_id?.slice(0, 20)}...`
+                                                : proposal?.author_id}
+                                            <span className="font-thin">| {timeAgo(proposal.ts)}</span>
+                                        </small>
+                                        <span className="md:text-base text-xs">{truncateString(proposal.summary)}</span>
                                     </div>
-                                    <div className="flex flex-row gap-1 items-center">
-                                        <img width={15} src="/assets/icon/reply.svg" alt="icon" />
-                                        <small className="text-[#04A46E] font-semibold text-xs">3 replies</small>
+                                    <div className="flex flex-row gap-5 mt-3 items-center">
+                                        <div className="flex flex-row gap-1 items-center">
+                                            <img width={15} src="/assets/icon/heart.svg" alt="icon" />
+                                            <small className="text-[#E0023D] font-semibold text-xs">3 votes</small>
+                                        </div>
+                                        <div className="flex flex-row gap-1 items-center">
+                                            <img width={15} src="/assets/icon/reply.svg" alt="icon" />
+                                            <small className="text-[#04A46E] font-semibold text-xs">3 replies</small>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <button  
-                            style={{
-                                borderColor:
-                                timelineStyle[
-                                    JSON.parse(proposal?.timeline)?.status
-                                ],
-                            }}
-                            className="cursor-pointer border-aipgf-geyser border-[1px] border-solid box-border bg-white hover:bg-stone-50 h-8 p-1 px-4 rounded-full flex flex-row gap-1 items-center">
-                            {/* <img width={16} src="/assets/icon/pen.svg" alt="icon" /> */}
-                            <small
+                            <button  
                                 style={{
-                                    color:
+                                    borderColor:
                                     timelineStyle[
                                         JSON.parse(proposal?.timeline)?.status
                                     ],
                                 }}
-                            >
-                                {proposal?.timeline &&
-                                    JSON.parse(proposal?.timeline)
-                                        ?.status.replace("_", " ")
-                                        .toLowerCase()
-                                        .replace(/\b\w/g, (c: any) =>
-                                            c.toUpperCase()
-                                        )}
-                            </small>
-                        </button>
-                    </div>
-                </Link>
-            ))
+                                className="cursor-pointer border-aipgf-geyser border-[1px] border-solid box-border bg-white hover:bg-stone-50 h-8 p-1 px-4 rounded-full flex flex-row gap-1 items-center">
+                                {/* <img width={16} src="/assets/icon/pen.svg" alt="icon" /> */}
+                                <small
+                                    style={{
+                                        color:
+                                        timelineStyle[
+                                            JSON.parse(proposal?.timeline)?.status
+                                        ],
+                                    }}
+                                >
+                                    {proposal?.timeline &&
+                                        JSON.parse(proposal?.timeline)
+                                            ?.status.replace("_", " ")
+                                            .toLowerCase()
+                                            .replace(/\b\w/g, (c: any) =>
+                                                c.toUpperCase()
+                                            )}
+                                </small>
+                            </button>
+                        </div>
+                    </Link>
+                ))
+            )
         )
+    
+    }
+
+    return(
+        <div className="flex flex-col gap-3">
+            {renderProposals()}
+        </div>
     )
 }
 
